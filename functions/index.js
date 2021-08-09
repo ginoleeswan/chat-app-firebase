15370207dfcff6a7a99a87cba127c73467feb0e1
Gino Swanepoel <ginoleemusic@live.com>
2021-08-10 00:27:09 +0200

    minor changes

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,26 @@
-const functions = require("firebase-functions");
-const Filter = require("bad-words");
+// const functions = require("firebase-functions");
+// const Filter = require("bad-words");
 
-const admin = require("firebase-admin");
-admin.initializeApp();
+// const admin = require("firebase-admin");
+// admin.initializeApp();
 
-const db = admin.firestore();
+// const db = admin.firestore();
 
-exports.detectEvilUsers = functions.firestore
-  .document("messages/{msgID}")
-  .onCreate(async (doc, ctx) => {
-    const filter = new Filter();
-    const { text, uid } = doc.data();
+// exports.detectEvilUsers = functions.firestore
+//   .document("messages/{msgID}")
+//   .onCreate(async (doc, ctx) => {
+//     const filter = new Filter();
+//     const { text, uid } = doc.data();
 
-    if (filter.isProfane(text)) {
-      const cleaned = filter.clean(text);
-      await doc.ref.update({
-        text: `i got BANNED for life for saying... ${cleaned}`,
-      });
+//     if (filter.isProfane(text)) {
+//       const cleaned = filter.clean(text);
+//       await doc.ref.update({
+//         text: `i got BANNED for life for saying... ${cleaned}`,
+//       });
 
-      await db.collection("banned").doc(uid).set({});
-    }
-  });
+//       await db.collection("banned").doc(uid).set({});
+//     }
+//   });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
